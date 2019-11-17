0f7565d72ff908b37e854475bba1653c83753a66
jbenastey <jbenastey@gmail.com>
2019-11-18 00:00:26 +0700

    revisi grafik

--- assets/dist/js/grafik/grafik.js
+++ assets/dist/js/grafik/grafik.js
@@ -18,22 +18,89 @@ $(document).ready(function () {
 		cache: false,
 		dataType: 'json',
 		success: function (response) {
+			console.log(response);
 			var faktor = [];
 			var jumlahnya = [];
+			var jumlahfst = [];
+			var jumlahftk = [];
+			var jumlahfud = [];
+			var jumlahfps = [];
+			var jumlahfes = [];
+			var jumlahfsh = [];
+			var jumlahfdk = [];
+			var jumlahfpp = [];
 			for (var i = 0; i < response.faktor.length; i++) {
 				faktor.push(response.faktor[i].faktor_nama);
 			}
 			for (var k = 0; k < faktor.length; k++) {
-				var total = 0;
-				var hitung = 0;
+				var totalfst = 0;
+				var hitungfst = 0;
+				var totalftk = 0;
+				var hitungftk = 0;
+				var totalfud = 0;
+				var hitungfud = 0;
+				var totalfps = 0;
+				var hitungfps = 0;
+				var totalfes = 0;
+				var hitungfes = 0;
+				var totalfsh = 0;
+				var hitungfsh = 0;
+				var totalfdk = 0;
+				var totalfpp = 0;
+				var hitungfdk = 0;
+				var hitungfpp = 0;
 				for (var j = 0; j < response.grafik.length; j++) {
 					if (response.grafik[j].faktor_nama === faktor[k]) {
-						total = total + parseInt(response.grafik[j].detail_jawaban);
-						hitung++;
+						if (response.grafik[j].kuesioner_fakultas === 'fst'){
+							totalfst = totalfst + parseInt(response.grafik[j].detail_jawaban);
+							hitungfst++;
+						}
+						else if (response.grafik[j].kuesioner_fakultas === 'ftk'){
+							totalftk = totalftk + parseInt(response.grafik[j].detail_jawaban);
+							hitungftk++;
+						}
+						else if (response.grafik[j].kuesioner_fakultas === 'fud'){
+							totalfud = totalfud + parseInt(response.grafik[j].detail_jawaban);
+							hitungfud++;
+						}
+						else if (response.grafik[j].kuesioner_fakultas === 'fpsi'){
+							totalfps = totalfps + parseInt(response.grafik[j].detail_jawaban);
+							hitungfps++;
+						}
+						else if (response.grafik[j].kuesioner_fakultas === 'fekonsos'){
+							totalfes = totalfes + parseInt(response.grafik[j].detail_jawaban);
+							hitungfes++;
+						}
+						else if (response.grafik[j].kuesioner_fakultas === 'fasih'){
+							totalfsh = totalfsh + parseInt(response.grafik[j].detail_jawaban);
+							hitungfsh++;
+						}
+						else if (response.grafik[j].kuesioner_fakultas === 'fdk'){
+							totalfdk = totalfdk + parseInt(response.grafik[j].detail_jawaban);
+							hitungfdk++;
+						}
+						else if (response.grafik[j].kuesioner_fakultas === 'fapertapet'){
+							totalfpp = totalfpp + parseInt(response.grafik[j].detail_jawaban);
+							hitungfpp++;
+						}
 					}
 				}
-				var rata = total/hitung;
-				jumlahnya.push(rata)
+				var ratafst = totalfst/hitungfst;
+				jumlahfst.push(getNum(ratafst).toFixed(2));
+				var rataftk = totalftk/hitungftk;
+				jumlahftk.push(getNum(rataftk).toFixed(2));
+				var ratafud = totalfud/hitungfud;
+				jumlahfud.push(getNum(ratafud).toFixed(2));
+				var ratafps = totalfps/hitungfps;
+				jumlahfps.push(getNum(ratafps).toFixed(2));
+				var ratafes = totalfes/hitungfes;
+				jumlahfes.push(getNum(ratafes).toFixed(2));
+				var ratafsh = totalfsh/hitungfsh;
+				jumlahfsh.push(getNum(ratafsh).toFixed(2));
+				var ratafdk = totalfdk/hitungfdk;
+				jumlahfdk.push(getNum(ratafdk).toFixed(2));
+				var ratafpp = totalfpp/hitungfpp;
+				jumlahfpp.push(getNum(ratafpp).toFixed(2));
 			}
 			var salesChart = new Chart($salesChart, {
 				type: 'bar',
@@ -41,10 +108,52 @@ $(document).ready(function () {
 					labels: faktor,
 					datasets: [
 						{
-							label: 'Nilai',
+							label: 'FST',
 							backgroundColor: '#007bff',
 							borderColor: '#007bff',
-							data: jumlahnya
+							data: jumlahfst
+						},
+						{
+							label: 'FSH',
+							backgroundColor: '#04ff13',
+							borderColor: '#04ff13',
+							data: jumlahfsh
+						},
+						{
+							label: 'FTK',
+							backgroundColor: '#ff0070',
+							borderColor: '#ff0070',
+							data: jumlahftk
+						},
+						{
+							label: 'FUD',
+							backgroundColor: '#09f5ff',
+							borderColor: '#09f5ff',
+							data: jumlahfud
+						},
+						{
+							label: 'FES',
+							backgroundColor: '#ffd200',
+							borderColor: '#ffd200',
+							data: jumlahfes
+						},
+						{
+							label: 'FDK',
+							backgroundColor: '#eb00ff',
+							borderColor: '#eb00ff',
+							data: jumlahfdk
+						},
+						{
+							label: 'FPP',
+							backgroundColor: '#ffc94c',
+							borderColor: '#ffc94c',
+							data: jumlahfpp
+						},
+						{
+							label: 'FPS',
+							backgroundColor: '#ff00d8',
+							borderColor: '#ff00d8',
+							data: jumlahfps
 						}
 					]
 				},
@@ -88,22 +197,89 @@ $(document).ready(function () {
 		cache: false,
 		dataType: 'json',
 		success: function (response) {
+			console.log(response);
 			var faktor = [];
 			var jumlahnya = [];
+			var jumlahfst = [];
+			var jumlahftk = [];
+			var jumlahfud = [];
+			var jumlahfps = [];
+			var jumlahfes = [];
+			var jumlahfsh = [];
+			var jumlahfdk = [];
+			var jumlahfpp = [];
 			for (var i = 0; i < response.faktor.length; i++) {
 				faktor.push(response.faktor[i].faktor_nama);
 			}
 			for (var k = 0; k < faktor.length; k++) {
-				var total = 0;
-				var hitung = 0;
+				var totalfst = 0;
+				var hitungfst = 0;
+				var totalftk = 0;
+				var hitungftk = 0;
+				var totalfud = 0;
+				var hitungfud = 0;
+				var totalfps = 0;
+				var hitungfps = 0;
+				var totalfes = 0;
+				var hitungfes = 0;
+				var totalfsh = 0;
+				var hitungfsh = 0;
+				var totalfdk = 0;
+				var totalfpp = 0;
+				var hitungfdk = 0;
+				var hitungfpp = 0;
 				for (var j = 0; j < response.grafik.length; j++) {
 					if (response.grafik[j].faktor_nama === faktor[k]) {
-						total = total + parseInt(response.grafik[j].detail_jawaban);
-						hitung++;
+						if (response.grafik[j].kuesioner_fakultas === 'fst'){
+							totalfst = totalfst + parseInt(response.grafik[j].detail_jawaban);
+							hitungfst++;
+						}
+						else if (response.grafik[j].kuesioner_fakultas === 'ftk'){
+							totalftk = totalftk + parseInt(response.grafik[j].detail_jawaban);
+							hitungftk++;
+						}
+						else if (response.grafik[j].kuesioner_fakultas === 'fud'){
+							totalfud = totalfud + parseInt(response.grafik[j].detail_jawaban);
+							hitungfud++;
+						}
+						else if (response.grafik[j].kuesioner_fakultas === 'fpsi'){
+							totalfps = totalfps + parseInt(response.grafik[j].detail_jawaban);
+							hitungfps++;
+						}
+						else if (response.grafik[j].kuesioner_fakultas === 'fekonsos'){
+							totalfes = totalfes + parseInt(response.grafik[j].detail_jawaban);
+							hitungfes++;
+						}
+						else if (response.grafik[j].kuesioner_fakultas === 'fasih'){
+							totalfsh = totalfsh + parseInt(response.grafik[j].detail_jawaban);
+							hitungfsh++;
+						}
+						else if (response.grafik[j].kuesioner_fakultas === 'fdk'){
+							totalfdk = totalfdk + parseInt(response.grafik[j].detail_jawaban);
+							hitungfdk++;
+						}
+						else if (response.grafik[j].kuesioner_fakultas === 'fapertapet'){
+							totalfpp = totalfpp + parseInt(response.grafik[j].detail_jawaban);
+							hitungfpp++;
+						}
 					}
 				}
-				var rata = total/hitung;
-				jumlahnya.push(rata)
+				var ratafst = totalfst/hitungfst;
+				jumlahfst.push(getNum(ratafst).toFixed(2));
+				var rataftk = totalftk/hitungftk;
+				jumlahftk.push(getNum(rataftk).toFixed(2));
+				var ratafud = totalfud/hitungfud;
+				jumlahfud.push(getNum(ratafud).toFixed(2));
+				var ratafps = totalfps/hitungfps;
+				jumlahfps.push(getNum(ratafps).toFixed(2));
+				var ratafes = totalfes/hitungfes;
+				jumlahfes.push(getNum(ratafes).toFixed(2));
+				var ratafsh = totalfsh/hitungfsh;
+				jumlahfsh.push(getNum(ratafsh).toFixed(2));
+				var ratafdk = totalfdk/hitungfdk;
+				jumlahfdk.push(getNum(ratafdk).toFixed(2));
+				var ratafpp = totalfpp/hitungfpp;
+				jumlahfpp.push(getNum(ratafpp).toFixed(2));
 			}
 			var salesChart = new Chart($salesChart2, {
 				type: 'bar',
@@ -111,10 +287,52 @@ $(document).ready(function () {
 					labels: faktor,
 					datasets: [
 						{
-							label: 'Nilai',
-							backgroundColor: '#00ff30',
-							borderColor: '#00ff30',
-							data: jumlahnya
+							label: 'FST',
+							backgroundColor: '#007bff',
+							borderColor: '#007bff',
+							data: jumlahfst
+						},
+						{
+							label: 'FSH',
+							backgroundColor: '#04ff13',
+							borderColor: '#04ff13',
+							data: jumlahfsh
+						},
+						{
+							label: 'FTK',
+							backgroundColor: '#ff0070',
+							borderColor: '#ff0070',
+							data: jumlahftk
+						},
+						{
+							label: 'FUD',
+							backgroundColor: '#09f5ff',
+							borderColor: '#09f5ff',
+							data: jumlahfud
+						},
+						{
+							label: 'FES',
+							backgroundColor: '#ffd200',
+							borderColor: '#ffd200',
+							data: jumlahfes
+						},
+						{
+							label: 'FDK',
+							backgroundColor: '#eb00ff',
+							borderColor: '#eb00ff',
+							data: jumlahfdk
+						},
+						{
+							label: 'FPP',
+							backgroundColor: '#ffc94c',
+							borderColor: '#ffc94c',
+							data: jumlahfpp
+						},
+						{
+							label: 'FPS',
+							backgroundColor: '#ff00d8',
+							borderColor: '#ff00d8',
+							data: jumlahfps
 						}
 					]
 				},
@@ -150,4 +368,11 @@ $(document).ready(function () {
 			console.log(response.status + 'error');
 		}
 	});
+
+	function getNum(val) {
+		if (isNaN(val)) {
+			return 0;
+		}
+		return val;
+	}
 })
